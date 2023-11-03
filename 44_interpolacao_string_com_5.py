32ea8209cf4d7772479af8a7e3394e82c1985a38
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-11-02 22:10:40 -0300

    Create 44_interpolacao_string_com_5.py

---
+++ 44_interpolacao_string_com_5.py
@@ -0,0 +1,12 @@
+"""
+interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Andressa'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
